627c4b858d07bf7342e9641e68c2c169c2d3b0bb
hungthehunter <133243197+hungthehunter@users.noreply.github.com>
2023-12-13 10:50:03 +0700

    product update

--- cardNVDIA/main.js
+++ cardNVDIA/main.js
@@ -28,30 +28,37 @@ function updateUI() {
 
     if (isLoggedIn()) {
         userSpan.style.display = 'inline';
-        loginButton.innerText = 'Đăng xuất';
+        loginButton.innerText = 'Log out';
     } else {
         userSpan.style.display = 'none';
-        loginButton.innerText = 'Đăng nhập';
+        loginButton.innerText = 'Log in';
     }
 }
 
 function isLoggedIn() {
     return localStorage.getItem('userEmail') !== null;
 }
-// main.js
+
 function login() {
-    // Lấy giá trị email từ input
+    // Lấy giá trị email và password từ input
     var userEmail = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-    // Kiểm tra nếu email không trống
-    if (userEmail !== '') {
-        // Lưu trạng thái đăng nhập và thông tin người dùng
-        localStorage.setItem('userEmail', userEmail);
+    // Kiểm tra nếu email không trống và password là '1'
+    if (userEmail !== '' && password === '1') {
+        // Kiểm tra nếu là email đặc biệt
+        if (userEmail === 'lenguyenquochunghello@gmail.com') {
+            // Chuyển hướng về trang quản trị
+            window.location.href = 'userAdmin.html';
+        } else {
+            // Lưu trạng thái đăng nhập và thông tin người dùng
+            localStorage.setItem('userEmail', userEmail);
 
-        // Chuyển hướng về trang chính
-        window.location.href = 'index.html';
+            // Chuyển hướng về trang chính
+            window.location.href = 'index.html';
+        }
     } else {
-        // Hiển thị thông báo lỗi nếu email trống
-        alert('Vui lòng nhập địa chỉ email.');
+        // Hiển thị thông báo lỗi nếu email trống hoặc password không đúng
+        alert('Email hoặc mật khẩu không đúng.');
     }
 }
